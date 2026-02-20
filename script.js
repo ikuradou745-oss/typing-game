dcfb42e084598344e803e1b697372c22129a69bb
ikuradou745-oss <ikuradou745@gmail.com>
2026-02-20 21:03:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -655,7 +655,7 @@ window.activateSkill = () => {
         showBattleAlert("👊 パンチ発動！", "var(--accent-red)");
     } 
     else if (skill.id === "autotype") {
-        startAutoTypeEngine(3000, 70); // 0.5秒で5文字(100ms間隔)
+        startAutoTypeEngine(3000, 1); // 0.5秒で5文字(100ms間隔)
         showBattleAlert("⚡ 自動入力発動！", "var(--accent-blue)");
     } 
     else if (skill.id === "comboUp") {
